c14b52573846e6eb9be1a9703e8a3cf60cd3cf00
Ayesha-here <ayeshakumari1919@gmail.com>
2026-06-16 23:54:38 +0530

    Add signup form validation and avatar upload

--- frontend/job-portal/src/pages/Auth/Login.jsx
+++ frontend/job-portal/src/pages/Auth/Login.jsx
@@ -8,7 +8,8 @@ import {
   Loader,
   AlertCircle,
   CheckCircle
-} from 'lucide-react'
+} from 'lucide-react';
+import { validateEmail } from "../../utils/helper";
 
 const Login = () => {
 
@@ -23,14 +24,6 @@ const Login = () => {
     showPassword: false,
     success: false
   });
-
-  //Validation functions
-  const validateEmail = (email) => {
-      if (!email.trim()) return 'Email is required';
-      const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+/;
-      if (!emailRegex.test(email)) return 'please enter a valid email address';
-      return '';
-  };
 
   const validatePassword = (password) => {
       if (!password) return 'Password is required';
